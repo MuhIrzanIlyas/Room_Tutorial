1bb72c0a7fcbd9c1fec53f242c1b183d5a44e70b
Muh. Irzan Ilyas <ilyasmi18d@student.unhas.ac.id>
2021-05-25 22:28:26 +0800

    Dao

--- app/src/main/java/id/ac/unhas/room_mvvm_coroutines_livedata/NoteDao.kt
+++ app/src/main/java/id/ac/unhas/room_mvvm_coroutines_livedata/NoteDao.kt
@@ -1,5 +1,19 @@
 package id.ac.unhas.room_mvvm_coroutines_livedata
 
-class NoteDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
+@Dao
+interface NoteDao {
+    @Query("Select * from note")
+    fun getNotes(): LiveData<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
 }
